43a463afa4b45a3ab76f8e00a0d9c09f85e0770d
Furuzono Makoto <freezemark5166@gmail.com>
2018-08-15 08:55:09 +0900

    test

---
+++ Quake_Visualization/js/qv_utils.js
@@ -0,0 +1,185 @@
+function display_resize(gl, id){
+    var dpr = 1.0;
+    var c = document.getElementById(id);
+    c.width = window.innerWidth * dpr;
+    c.height = window.innerHeight * dpr;
+    gl.viewport(0.0, 0.0, c.width, c.height);
+}
+
+function create_framebuffer(gl, width, height){
+    var frameBuffer = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuffer);
+    var depthRenderBuffer = gl.createRenderbuffer();
+    gl.bindRenderbuffer(gl.RENDERBUFFER, depthRenderBuffer);
+    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, width, height);
+    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthRenderBuffer);
+    var fTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, fTexture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, fTexture, 0);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    return {f : frameBuffer, d : depthRenderBuffer, t : fTexture};
+}
+
+function japan(file){
+    var coord = new Array(), alpha = new Array();
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", file, false);
+    xhr.send(null);
+    var alpha_flag = false;
+    
+    var tmpArray = xhr.responseText.split("\n");
+    for(var i = 0; i < tmpArray.length; i++){
+        var word = ' @' + tmpArray[i];
+        if(word.indexOf(' @' + '#') !=-1){
+            console.log("japan program started.");
+        }else if(word.indexOf(' @' + '>') !=-1){
+            if(coord.length === 0){
+                continue;
+            }else{
+                coord.push(coord[coord.length - 1], coord[coord.length]);
+                alpha.push(0.0);
+                alpha_flag = true;
+            }
+        }else if(i + 1 === tmpArray.length){
+            console.log("japan program finished.");
+        }else{
+            var vertex = tmpArray[i].split(String.fromCharCode(9));
+            if(alpha_flag = true){
+                coord.push(vertex[0], vertex[1]);
+                alpha.push(0.0);
+                alpha_flag = false;
+            }
+            coord.push(vertex[0], vertex[1]);
+            alpha.push(1.0);
+        }
+    }
+    
+    var pos = new Array(), col = new Array(), ani = new Array();
+    
+    var cnt = 0;
+    for(var i = 0; i < coord.length; i += 2){
+        pos.push(coord[i], coord[i + 1], 0.0);
+        ani.push(0.1);
+        col.push(1.0, 1.0, 1.0, alpha[cnt]);
+        cnt++
+    }
+    
+    return {p : pos, c : col, a : ani};
+}
+
+function quake(today){
+    //var file = 'data/h201610';
+    var file = 'data/h2011';
+    var pos = new Array(), col = new Array(), dep = new Array();
+    var year, month, day, date, verX, verXX, verY, verYY, depth, depthDep, mag, sint;
+    var mFlag = true, sFlag = false;
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", file, false);
+    xhr.send(null);
+    
+    var tmpArray = xhr.responseText.split("\n");
+    for(var i = 0; i < tmpArray.length; i++){
+        year = tmpArray[i].substr(1, 4);
+        month = tmpArray[i].substr(5, 2);
+        day = tmpArray[i].substr(7, 2);
+        date = year + "-" + month + "-" + day;
+        verX = tmpArray[i].substr(21, 7);
+        verXX = tmpArray[i].substr(28, 4);
+        verY = tmpArray[i].substr(32, 8);
+        verYY = tmpArray[i].substr(40, 4);
+        depth = tmpArray[i].substr(44, 5);
+        depthDep = tmpArray[i].substr(49, 3);
+        mag = parseFloat(tmpArray[i].substr(52, 2) * 0.1);
+        sint = tmpArray[i].substr(61, 1);
+        
+        if(date == today && mFlag == true){
+            if(mag >= 3.0 && mag < 4.0){
+                col.push(0.0, 0.0, 1.0, 1.0);
+                pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep);
+            }
+            else if(mag >= 4.0 && mag < 5.0){
+                col.push(1.0, 0.8, 0.6, 1.0);
+                pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep);
+            }
+            else if(mag >= 5.0 && mag < 6.0){
+                col.push(1.0, 1.0, 0.0, 1.0);
+                pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep);
+            }
+            else if(mag >= 6.0 && mag < 7.0){
+                col.push(1.0, 0.5, 0.0, 1.0);
+                pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep);
+            }
+            else if(mag >= 7.0 && mag < 8.0){
+                col.push(1.0, 0.0, 0.0, 1.0);
+                pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep);
+            }
+            else if(mag >= 8.0 && mag < 9.0){
+                col.push(0.5, 0.0, 0.1, 1.0);
+                pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep);
+            }
+            else if(mag >= 9.0){
+                col.push(0.5, 0.0, 0.5, 1.0);
+                pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep);
+            }
+        }
+        else if(date == today && sFlag == true){
+            if(sint == 3){
+                col.push(0.0, 0.0, 1.0, 1.0);
+                pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep);
+            }
+            else if(sint == 4){
+                col.push(1.0, 0.8, 0.6, 1.0);
+                pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep);
+            }
+            else if(sint == "A"){
+                col.push(1.0, 1.0, 0.0, 1.0);
+                pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep);
+            }
+            else if(sint == "B"){
+                col.push(1.0, 0.5, 0.0, 1.0);
+                pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep);
+            }
+            else if(sint == "C"){
+                col.push(1.0, 0.0, 0.0, 1.0);
+                pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep);
+            }
+            else if(sint == "D"){
+                col.push(0.5, 0.0, 0.1, 1.0);
+                pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep);
+            }
+            else if(sint == 7){
+                col.push(0.5, 0.0, 0.5, 1.0);
+                pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep);
+            }
+        }
+    }
+    
+    return {p : pos, c : col, d : dep};
+}
+
+function pushdata(pos, dep, verX, verXX, verY, verYY, depth, depthDep){
+    pos.push(parseFloat(verY * 0.0001 + 2 * verYY * 0.01), parseFloat(verX * 0.0001 + 2 * verXX * 0.01), 0.001);
+    dep.push(parseFloat(depth * 0.01 + 2 * depthDep * 0.01));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Quake_Visualization/js/script.js
@@ -0,0 +1,241 @@
+onload = function(){
+    var date = new Date();
+
+    var day = date.getDate();
+    var month = date.getMonth();
+    var year = date.getFullYear() - 1;
+
+    if(month < 10) month = "0" + month;
+    if(month == 0){
+        month = "12";
+        year = year - 1;
+    }
+    if(day < 10) day = "0" + day;
+
+    //var today = year + "-" + month + "-" + day;
+    var today = "2011-03-11";
+    document.getElementById("today").value = today;
+    
+    var JaLine = japan('gmt/japan.gmt');
+    var QuData = quake(today);
+    
+    var c = document.getElementById('canvas');
+    var gl = c.getContext('webgl') || c.getContext('experimental-webgl');
+    if(!gl) alert("It failed making gl.");
+    
+    display_resize(gl, 'canvas');
+    
+    //CameraWork
+    var default_camPosition = [139.766667, 35.681111, 30.0];
+    var default_camLookAt = [139.766667, 35.681111, 0.0];
+    var defaultcamUp = [0.0, 1.0, 0.0];
+    var camPosition = default_camPosition;
+    var camLookAt = default_camLookAt;
+    var camUp = defaultcamUp;
+    
+    var m = new matIV();
+    var mMatrix = m.identity(m.create());
+    var vMatrix = m.identity(m.create());
+    var pMatrix = m.identity(m.create());
+    var tmpMatrix = m.identity(m.create());
+    var mvpMatrix = m.identity(m.create());
+    var invMatrix = m.identity(m.create());
+    
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.enable(gl.BLEND);
+    gl.enable(gl.CULL_FACE);
+    
+    var main_vs = create_shader(gl, 'main_vs');
+    var main_fs = create_shader(gl, 'main_fs');
+    var main_prg = create_program(gl, main_vs, main_fs);
+    var main_attL = new Array();
+    main_attL[0] = gl.getAttribLocation(main_prg, 'position');
+    main_attL[1] = gl.getAttribLocation(main_prg, 'color');
+    main_attL[2] = gl.getAttribLocation(main_prg, 'flag');
+    var main_attS = [3, 4, 1];
+    var main_uniL = new Array();
+    main_uniL[0] = gl.getUniformLocation(main_prg, 'mvpMatrix');
+    
+    var point_vs = create_shader(gl, 'point_vs');
+    var point_fs = create_shader(gl, 'point_fs');
+    var point_prg = create_program(gl, point_vs, point_fs);
+    var point_attL = new Array();
+    point_attL[0] = gl.getAttribLocation(point_prg, 'position');
+    point_attL[1] = gl.getAttribLocation(point_prg, 'color');
+    var point_attS = [3, 4];
+    var point_uniL = new Array();
+    point_uniL[0] = gl.getUniformLocation(point_prg, 'mvpMatrix');
+    point_uniL[1] = gl.getUniformLocation(point_prg, 'pointSize');
+    
+//Japan Line
+    var JLPos = create_vbo(gl, JaLine.p);
+    var JLCol = create_vbo(gl, JaLine.c);
+    var JLAni = create_vbo(gl, JaLine.a);
+    var JLVBOList = [JLPos, JLCol, JLAni];
+    
+//Quake Point
+    var QuPos = create_vbo(gl, QuData.p);
+    var QuCol = create_vbo(gl, QuData.c);
+    var QuVBOList = [QuPos, QuCol];
+    
+//frame buffer
+    var bufferWidth = 512, bufferHeight = 512;
+    
+    gl.activeTexture(gl.TEXTURE1);
+    gl.activeTexture(gl.TEXTURE2);
+    gl.activeTexture(gl.TEXTURE3);
+    gl.activeTexture(gl.TEXTURE4);
+    gl.activeTexture(gl.TEXTURE5);
+    var fBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+    var bBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+    var rBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+    var gBuffer1 = create_framebuffer(gl, bufferWidth, bufferHeight);
+    var gBuffer2 = create_framebuffer(gl, bufferWidth, bufferHeight);
+    
+    update_framebuffer();
+    
+    function update_framebuffer(){
+        if(c.width <= 512 || c.height <= 512){
+            bufferWidth = 512;
+            bufferHeight = 512;
+            fBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            bBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            rBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            gBuffer1 = create_framebuffer(gl, bufferWidth, bufferHeight);
+            gBuffer2 = create_framebuffer(gl, bufferWidth, bufferHeight);
+        }else if((c.width > 512 || c.height > 512) && (c.width <= 1024 || c.height <= 1024)){
+            bufferWidth = 1024;
+            bufferHeight = 1024;
+            fBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            bBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            rBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            gBuffer1 = create_framebuffer(gl, bufferWidth, bufferHeight);
+            gBuffer2 = create_framebuffer(gl, bufferWidth, bufferHeight);
+        }else if((c.width > 1024 || c.height > 1024) && (c.width <= 2048 || c.height <= 2048)){
+            bufferWidth = 2048;
+            bufferHeight = 2048;
+            fBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            bBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            rBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            gBuffer1 = create_framebuffer(gl, bufferWidth, bufferHeight);
+            gBuffer2 = create_framebuffer(gl, bufferWidth, bufferHeight);
+        }else if((c.width > 2048 || c.height > 2048) && (c.width <= 4096 || c.height <= 4096)){
+            bufferWidth = 4096;
+            bufferHeight = 4096;
+            fBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            bBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            rBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            gBuffer1 = create_framebuffer(gl, bufferWidth, bufferHeight);
+            gBuffer2 = create_framebuffer(gl, bufferWidth, bufferHeight);
+        }else if((c.width > 4096 || c.height > 4096) && (c.width <= 8192 || c.height <= 8192)){
+            bufferWidth = 8192;
+            bufferHeight = 8192;
+            fBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            bBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            rBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            gBuffer1 = create_framebuffer(gl, bufferWidth, bufferHeight);
+            gBuffer2 = create_framebuffer(gl, bufferWidth, bufferHeight);
+        }else if((c.width > 8192 || c.height > 8192) && (c.width <= 16384 || c.height <= 16384)){
+            bufferWidth = 16384;
+            bufferHeight = 16384;
+            fBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            bBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            rBuffer = create_framebuffer(gl, bufferWidth, bufferHeight);
+            gBuffer1 = create_framebuffer(gl, bufferWidth, bufferHeight);
+            gBuffer2 = create_framebuffer(gl, bufferWidth, bufferHeight);
+        }
+    }
+    
+    window.onresize = function(){
+        display_resize(gl, 'canvas');
+        update_framebuffer();
+    }
+    
+    var drag = false;
+    var m_stX = 0, m_stY = 0;
+    
+    document.addEventListener('mousedown' || 'touchstart', function(e){
+        e.preventDefault();
+        drag = true;
+        m_stX = e.clientX;
+        m_stY = e.clientY;
+    }, true);
+    
+    document.addEventListener('mousemove' || 'touchend', function(e){
+        if(drag){
+            var safety = camPosition[2] / 15.0;
+            var ratio = 0.0005;
+            
+            camPosition[0] = camLookAt[0] - ((-m_stX + e.clientX) * safety) * ratio;
+            camPosition[1] = camLookAt[1] + ((-m_stY + e.clientY) * safety) * ratio;
+            
+            camLookAt[0] = camPosition[0];
+            camLookAt[1] = camPosition[1];
+        }
+    }, true);
+    
+    document.addEventListener('mouseup' || 'touchmove', function(e){
+        drag = false;
+    }, true);
+    
+    document.addEventListener('wheel', function(e){
+        var del = 1.025;
+        if (e.shiftKey) del = 1.01;
+        var ds = ((e.deltaY || e.wheelDelta) > 0) ? del : (1 / del);
+        camPosition[2] /= ds;
+    }, true);
+    
+    (function(){
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearDepth(1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        
+        m.lookAt(camPosition, camLookAt, camUp, vMatrix);
+        m.perspective(45.0, c.width / c.height, 0.001, 1000.0, pMatrix);
+        m.multiply(pMatrix, vMatrix, tmpMatrix);
+        m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+        m.inverse(mMatrix, invMatrix);
+        
+        gl.useProgram(main_prg);
+        set_attribute(gl, JLVBOList, main_attL, main_attS);
+        gl.uniformMatrix4fv(main_uniL[0], false, mvpMatrix);
+        gl.drawArrays(gl.LINE_STRIP, 0, JaLine.p.length / 3);
+        
+        if(today != document.getElementById("today").value){
+            QuData = quake(document.getElementById("today").value);
+            today = document.getElementById("today").value;
+            
+            QuPos = create_vbo(gl, QuData.p);
+            QuCol = create_vbo(gl, QuData.c);
+            QuVBOList = [QuPos, QuCol];
+        }
+        
+        gl.useProgram(point_prg);
+        set_attribute(gl, QuVBOList, point_attL, point_attS);
+        gl.uniformMatrix4fv(point_uniL[0], false, mvpMatrix);
+        gl.uniform1f(point_uniL[1], 10.0);
+        gl.drawArrays(gl.POINTS, 0, QuData.p.length/ 3);
+        
+        gl.flush();
+		requestAnimationFrame(arguments.callee);
+        
+    })();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
